e4d756f05ebfd2668d946da9feb49af96a2ab544
daneryl <daneryl@gmail.com>
2013-07-26 22:13:05 -0500

    end and start tag now are options

--- tasks/jslinker.js
+++ tasks/jslinker.js
@@ -5,15 +5,18 @@ var util = require('util');
 module.exports = function(grunt) {
 
 	grunt.registerMultiTask('jslinker', 'Autoinsert script tags (or other filebased tags) in an html file', function() {
-    var options = this.options({target:"", exclude:[]});
+    var options = this.options({
+      target:"", 
+      exclude:[],
+      start_scripts_tag: "<!--SCRIPTS-->",
+      end_scripts_tag: "<!--SCRIPTS END-->"
+    });
 
-    var start_scripts_tag = "<!--SCRIPTS-->";
-    var end_scripts_tag = "<!--SCRIPTS END-->";
 
     var page = grunt.file.read(options.target);
 
-    var start = page.indexOf(start_scripts_tag);
-    var end = page.indexOf(end_scripts_tag, start);
+    var start = page.indexOf(options.start_scripts_tag);
+    var end = page.indexOf(options.end_scripts_tag, start);
 
     var excluded_files = grunt.file.expand(options.exclude);
     var scripts = "\n";
@@ -26,7 +29,7 @@ module.exports = function(grunt) {
       scripts += "<script src='"+file_path+"'></script>\n";
     });
 
-    var newPage = page.substr(0, start + start_scripts_tag.length) + scripts + page.substr(end);
+    var newPage = page.substr(0, start + options.start_scripts_tag.length) + scripts + page.substr(end);
     grunt.file.write(options.target, newPage);
   });
 
